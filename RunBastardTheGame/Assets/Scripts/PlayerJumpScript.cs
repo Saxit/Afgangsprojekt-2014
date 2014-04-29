a77b8019eff2bff474eb85488bbf47ec7b4eea6f
TokeOlsen <saxit_devil@hotmail.com>
2014-04-29 13:03:12 +0200

    Small locomotion og player jump script

---
+++ RunBastardTheGame/Assets/Scripts/PlayerJumpScript.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerJumpScript : MonoBehaviour {
+
+	public float JumpSpeed = 300.0f;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetButtonDown("Jump"))
+			Jump();
+	}
+
+	void Jump() {
+		rigidbody.AddForce(Vector3.up *JumpSpeed);
+	}
+}
