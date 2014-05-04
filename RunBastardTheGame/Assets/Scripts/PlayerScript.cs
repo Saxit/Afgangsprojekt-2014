1dcbde54ba184a7f01860b1ef7f536b9ae041cbb
Toke Olsen <tokenolsen@gmail.com>
2014-05-05 00:48:46 +0200

    Sliding påbegyndt (ikke færdig)

--- RunBastardTheGame/Assets/Scripts/PlayerScript.cs
+++ RunBastardTheGame/Assets/Scripts/PlayerScript.cs
@@ -6,19 +6,23 @@ public class PlayerScript : MonoBehaviour {
 
 	Animator anim;
 	CharacterController cc;
+    //BoxCollider boxCol;
 
 	public float jumpSpeed = 5.0f;
 	public float gravity = 20.0F;
-	//public float runSpeed = 1.0f;
+    public float slideHeight;
+    public bool isSliding = false;
 	private Vector3 moveDirection = Vector3.zero;
 
 	int jumpHash = Animator.StringToHash("Jump");
 	int groundedHash = Animator.StringToHash("IsGrounded");
+    int slideHash = Animator.StringToHash("Slide");
 
 	// Use this for initialization
 	void Start () {
 		anim = GetComponent<Animator>();
 		cc = GetComponent<CharacterController>();
+        //boxCol = GetComponent<BoxCollider>();
 	}
 	
 	// Update is called once per frame
@@ -27,19 +31,31 @@ public class PlayerScript : MonoBehaviour {
 		float move = Input.GetAxis("Vertical");
 		anim.SetFloat("Speed", move);
 
-		//rigidbody.AddForce(transform.forward * runSpeed);
-
-		if(Input.GetKeyDown(KeyCode.Space) && cc.isGrounded)
+		
+        //Jumping
+		if(Input.GetKeyDown(KeyCode.Space) && cc.isGrounded && !isSliding)
 		{
 
 			Jump();
 			anim.SetTrigger(jumpHash);
 		}
 
+
 		if(cc.isGrounded)
 		{
 			anim.SetBool(groundedHash, true);
+
+            if (Input.GetButton("Fire1")) 
+            {
+                Slide();
+            }
+
+            if (Input.GetButtonDown("Fire2")) 
+            {
+                StandUp();
+            }
 		}
+
 		moveDirection.y -= gravity * Time.deltaTime;
 		cc.Move(moveDirection * Time.deltaTime);
 	}
@@ -49,5 +65,20 @@ public class PlayerScript : MonoBehaviour {
 		moveDirection.y = jumpSpeed;
 		rigidbody.AddForce(Vector3.up * jumpSpeed);
 	}
+
+    void Slide()
+    {    
+        cc.height = 1f;
+        cc.center = new Vector3(0, 0.5f, 0);
+        isSliding = true;
+        anim.SetTrigger(slideHash);
+    }
+
+    void StandUp()
+    {
+        cc.height = 2f;
+        cc.center = new Vector3(0, 1f, 0);
+        isSliding = false;
+    }
 
 }
