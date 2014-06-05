36665d19fd016e5388e554989ca65faf0b105e49
TokeOlsen <saxit_devil@hotmail.com>
2014-06-05 13:04:39 +0200

    Update PlayerShoot

--- RunBastardTheGame/Assets/Scripts/PlayerShoot.cs
+++ RunBastardTheGame/Assets/Scripts/PlayerShoot.cs
@@ -4,39 +4,33 @@ using System.Collections.Generic;
 
 public class PlayerShoot : MonoBehaviour
 {
-    public Rigidbody projectile;
+    //public Rigidbody projectile;
     public Transform shooterPos;
 
     public GameObject obj;              //Listen over mulige spawnobjekter
     public float pooledAmount = 20;     //Max antal objekter i listen
     private List<GameObject> list;
 
+    void Awake()
+    {
+        PoolSpawns();
+    }
+
     void Update ()
     {
-        Vector3 endPos = new Vector3(0, shooterPos.position.y + 30, 0);
+        Vector3 endPos = new Vector3(this.transform.position.x, this.transform.position.y, this.transform.position.z - 30);
         
         if (Input.GetButtonUp("Fire1"))
         {
             GameObject bullet = Spawn();            
             
-            transform.position = Vector3.Lerp(shooterPos.position, endPos, Time.deltaTime);
+            bullet.transform.position = Vector3.Lerp(this.transform.position, endPos, Time.deltaTime);
         }
     }
 
     private void PoolSpawns()
     {
         list = new List<GameObject>();
-
-        //for (int i = 0; i < pooledAmount; i++)
-        //{
-        //    for (int n = 0; n < obj.Length; n++)
-        //    {
-
-        //        GameObject go = (GameObject)Instantiate(obj[n], transform.position, Quaternion.identity);
-        //        go.SetActive(false);
-        //        list.Add(go);
-        //    }
-        //}
 
         for (int i = 0; i < pooledAmount; i++)
         {
@@ -51,6 +45,7 @@ public class PlayerShoot : MonoBehaviour
 
     private GameObject Spawn()
     {
+
         GameObject isActive = null;
         for (int i = 0; i < list.Count; i++)
         {
@@ -60,6 +55,7 @@ public class PlayerShoot : MonoBehaviour
                 list[i].transform.position = this.transform.position;
                 list[i].SetActive(true);
                 isActive = list[i];
+                break;
                 
             }
         }
