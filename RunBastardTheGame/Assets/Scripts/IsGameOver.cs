6842842760fb2392b3b181826bfbae8082939331
Ronnie Hemmingsen <ronnie.hemmingsen@gmail.com>
2014-06-05 10:16:24 +0200

    more assets

---
+++ RunBastardTheGame/Assets/Scripts/IsGameOver.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class IsGameOver : MonoBehaviour {
+
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "LevelEnd")
+        {
+            Application.LoadLevel("LvlTrainWorld");
+            Debug.Log("Yay!");
+        }
+        else if (other.gameObject.tag == "Bullet")
+        {
+            Application.LoadLevel("LvlTrainWorld");
+            Debug.Log("Boo!");
+        }
+    }
+}
---
+++ RunBastardTheGame/Assets/Scripts/Level/IsLevelEnd.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class IsLevelEnd : MonoBehaviour {
+
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            Application.LoadLevel("MainMenu");
+        }
+    }
+}
