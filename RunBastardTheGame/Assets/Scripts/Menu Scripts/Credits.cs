09fac8437f4047ff20cd6f84d90218585be5dce0
Ronnie Hemmingsen <ronnie.hemmingsen@gmail.com>
2014-06-10 21:28:07 +0200

    smårettelser til projekt

--- RunBastardTheGame/Assets/Scripts/Menu Scripts/Credits.cs
+++ RunBastardTheGame/Assets/Scripts/Menu Scripts/Credits.cs
@@ -15,7 +15,7 @@ public class Credits : MonoBehaviour {
 
     void OnGUI()
     {
-        GUI.Box(new Rect(Screen.width / 2 - 100, Screen.height / 2 - 75, 200, 150), "<b>Zen Rabbit Studios</b> \n\n<b>Programmers</b> \nRonnie Hemmingsen \nToke Olsen \n\n<b>Graphics Design</b> \nNicolas");
+        GUI.Box(new Rect(Screen.width / 2 - 100, Screen.height / 2 - 75, 200, 150), "<b>Zen Rabbit Studios</b> \n\n<b>Programmers</b> \nRonnie Hemmingsen \nToke Olsen \n\n<b>Graphics Design</b> \nNicholas Alberg");
 
         GUILayout.BeginArea(new Rect(Screen.width / 2 - Screen.width / 4, Screen.height - Screen.height / 6, Screen.width / 2, Screen.height));
         GUILayout.BeginVertical("box");        
